b31ae6d467b463d433d18d1531c1c47805093ec0
akshat22s <akshatjais597@gmail.com>
2026-02-04 14:38:03 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (67.58%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -5,21 +5,19 @@ public:
         int high = nums.size()-1;
         while(low < high)
         {
-            int mid = low + (high - low)/2;
-            int len = high - mid;
+            int mid = low + (high-low)/2;
             bool isEven;
-            if(len % 2 == 0) isEven = true;
+            if((high-mid) % 2 == 0) isEven = true;
             else isEven = false;
-
             if(nums[mid] == nums[mid+1])
             {
-                if(isEven) low = mid+2;
-                else high = mid - 1;
+                if(isEven) low = mid+1;
+                else high = mid-1;
             }
             else
             {
                 if(isEven) high = mid;
-                else low = mid + 1;
+                else low = mid+1;
             }
         }
         return nums[low];
