bfb2dc1ae3aa09d5b69cb6379b07139c134a2c38
akshat22s <akshatjais597@gmail.com>
2026-02-18 12:21:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (86.38%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -3,14 +3,15 @@ public:
     int climbStairs(int n) {
         if(n <= 2) return n;
         int a = 1;
-        int b = 1;
+        int b = 2;
         int c;
-        for(int i = n-2; i >= 0; i--)
+
+        for(int i = 3; i <= n; i++)
         {
-            c = a + b;
+            c = a+b;
             a = b;
             b = c;
         }
-        return c;
+        return b;
     }
 };
