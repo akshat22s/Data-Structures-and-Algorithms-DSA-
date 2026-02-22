b02a09bc38968f73ff686cdae0e40fa718f0fab8
akshat22s <akshatjais597@gmail.com>
2026-02-22 23:55:41 +0530

    Time: 0 ms (100.00%) | Memory: 78.2 MB (65.69%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
@@ -1,39 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int SUM = 0;
-    long maxP = 0;
+    int totalSum;
+    long long maxP = 0;
     int helperSum(TreeNode* root)
     {
         if(root == NULL) return 0;
         return root->val + helperSum(root->left) + helperSum(root->right);
     }
-    int find(TreeNode* root)
+    int helper(TreeNode* root)
     {
         if(root == NULL) return 0;
-
-        int leftSum  = find(root->left);
-        int rightSum = find(root->right);
-
-        long subTreeSum = root->val + leftSum + rightSum; // S1
-        long remaningSubTreeSum = SUM - subTreeSum;       // S2
-
-        maxP = max(maxP, subTreeSum * remaningSubTreeSum);
-        return subTreeSum;
+        int leftSum  = helper(root->left);
+        int rightSum = helper(root->right);
+        long long subTree = root->val + leftSum + rightSum;
+        long long remainingSum = totalSum - subTree;
+        long long  product = subTree * remainingSum;
+        maxP = max(maxP, product);
+        return subTree;
     }
     int maxProduct(TreeNode* root) {
-        SUM = helperSum(root);
-        find(root);
+        totalSum = helperSum(root);
+        helper(root);
         return maxP % 1000000007;
     }
 };
