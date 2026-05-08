89cc0540ec1b829f77e16732320222351011c955
akshat22s <akshatjais597@gmail.com>
2026-05-08 21:35:41 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (40.23%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -1,15 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void preorder(TreeNode* root, int level, vector<int> &ans)
-    {
-        if(root == NULL) return;
-        if(ans.size() < level)  ans.push_back(root->val);
-        preorder(root->right,level+1, ans);
-        preorder(root->left, level+1, ans);
-    }
     vector<int> rightSideView(TreeNode* root) {
+        if(root == NULL) return {};
         vector<int> ans;
-        preorder(root, 1, ans);
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty())
+        {
+            int n = q.size();
+            int x = -1;
+            for(int i = 0; i < n; i++)
+            {
+                TreeNode* temp = q.front();
+                if(temp->left != NULL) q.push(temp->left);
+                if(temp->right != NULL) q.push(temp->right);
+                x = temp->val;
+                q.pop();
+            }
+            ans.push_back(x);
+        }
         return ans;
     }
 };
