31e1f897dfa004de09cd540410eee8bcd742254f
akshat22s <akshatjais597@gmail.com>
2026-06-07 17:35:20 +0530

    Time: 3 ms (70.82%) | Memory: 18.7 MB (39.56%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,53 +1,43 @@
 class Solution {
 public:
-    vector<int> topo(int V, unordered_map<int, vector<int>> &adj, vector<int> &ans)
-    {
-        queue<int> q;
-        vector<int> indegree(V, 0);
-
-        for(int u = 0; u < V; u++)
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> adj;
+        for(auto &vec : prerequisites)
+        {
+            int a = vec[0];
+            int b = vec[1];
+            adj[b].push_back(a);
+        }
+        vector<int> indegree(numCourses, 0);
+        for(int u = 0; u < numCourses; u++)
         {
             for(auto &v : adj[u])
             {
                 indegree[v]++;
             }
         }
-
-        for(int i = 0; i < V; i++)
+        queue<int> q;
+        for(int i = 0; i < numCourses; i++)
         {
-            if(indegree[i] == 0)
-            {
-                q.push(i);
-            }
+            if(indegree[i] == 0) q.push(i);
         }
 
+        vector<int> ans;
         while(!q.empty())
         {
             int u = q.front();
             q.pop();
             ans.push_back(u);
+
             for(auto &v : adj[u])
             {
                 indegree[v]--;
+
                 if(indegree[v] == 0) q.push(v);
             }
         }
-        return ans;
-    }
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-       unordered_map<int, vector<int>> adj;
-       for(auto &vec : prerequisites)
-       {
-            int a = vec[0];
-            int b = vec[1];
-
-            // b ----> a
-            // phela b karo uska baad a ko karo
-            adj[b].push_back(a);
-        }
-        vector<int> ans;
-        topo(numCourses, adj, ans);
         if(ans.size() == numCourses) return ans;
         else return {};
+        
     }
 };
